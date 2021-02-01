a761138b15e89124a09f69c733f24ae2c6af98b1
Mads Horsted Brunebjerg <mads81i7@edu.easj.dk>
2021-02-01 12:27:19 +0100

    Add project files.

---
+++ SpeedMeasuremetRazor/Services/SpeedMeasurementRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SpeedMeasuremetRazor.Helpers;
+using SpeedMeasuremetRazor.Interfaces;
+using SpeedMeasuremetRazor.Models;
+
+namespace SpeedMeasuremetRazor.Services
+{
+    public class SpeedMeasurementRepo 
+    {
+        
+    }
+}
