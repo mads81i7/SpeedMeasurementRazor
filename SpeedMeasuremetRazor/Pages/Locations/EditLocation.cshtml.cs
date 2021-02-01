d6e6023ef3d9d8a0270433abce22c957b8e38efb
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2021-02-01 14:18:00 +0100

    Edit user page implemented

--- SpeedMeasuremetRazor/Pages/Locations/EditLocation.cshtml.cs
+++ SpeedMeasuremetRazor/Pages/Locations/EditLocation.cshtml.cs
@@ -4,13 +4,43 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SpeedMeasuremetRazor.Interfaces;
+using SpeedMeasuremetRazor.Models;
 
 namespace SpeedMeasuremetRazor.Pages.Locations
 {
     public class EditLocationModel : PageModel
     {
-        public void OnGet()
+        private readonly ILocationRepo _locations;
+        [BindProperty]
+        public new Location Location { get; set; }
+
+        public EditLocationModel(ILocationRepo locations)
         {
+            _locations = locations;
+        }
+
+        public IActionResult OnGet(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Location = _locations.GetLocation((int)id);
+
+            return Page();
+        }
+
+        public IActionResult OnPost(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            _locations.UpdateLocation(Location);
+            return RedirectToPage("./Index");
         }
     }
 }
