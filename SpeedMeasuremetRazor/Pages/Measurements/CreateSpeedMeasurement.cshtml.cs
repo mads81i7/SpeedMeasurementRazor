2f708a2e9c74ae79b2de2fa48a94d429daeffdd0
Mads Horsted Brunebjerg <mads81i7@edu.easj.dk>
2021-02-03 12:22:27 +0100

    Create speed measurement begyndt

---
+++ SpeedMeasuremetRazor/Pages/Measurements/CreateSpeedMeasurement.cshtml.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SpeedMeasuremetRazor.Interfaces;
+using SpeedMeasuremetRazor.Models;
+
+namespace SpeedMeasuremetRazor.Pages.Measurements
+{
+    public class CreateSpeedMeasurementModel : PageModel
+    {
+        private ISpeedMeasurementRepo _measurements;
+        private ILocationRepo _locations;
+        public List<SpeedMeasurement> Measurements { get; set; }
+        public List<SelectListItem> Options { get; set; }
+
+        [BindProperty]
+        public SpeedMeasurement SpeedMeasurement { get; set; }
+        [BindProperty]
+        public int LocationId { get; set; }
+
+        public CreateSpeedMeasurementModel(ISpeedMeasurementRepo repo, ILocationRepo locationRepo)
+        {
+            _measurements = repo;
+            _locations = locationRepo;
+        }
+        public void OnGet()
+        {
+            Measurements = _measurements.GetAllSpeedMeasurements();
+            Options = _locations.GetAllLocations().Select(a => new SelectListItem
+            {
+                Value = a.Id.ToString(),
+                Text = a.Address
+            }).ToList();
+        }
+    }
+}
