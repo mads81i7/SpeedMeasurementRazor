fc672a77eaa0bcd17042836bbdd20f07345190a2
Mads Horsted Brunebjerg <mads81i7@edu.easj.dk>
2021-02-18 15:20:35 +0100

    Filter på speedmeasurement index

--- SpeedMeasuremetRazor/Pages/Measurements/Index.cshtml.cs
+++ SpeedMeasuremetRazor/Pages/Measurements/Index.cshtml.cs
@@ -4,21 +4,66 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SpeedMeasuremetRazor.Helpers;
 using SpeedMeasuremetRazor.Interfaces;
+using SpeedMeasuremetRazor.Models;
 
 namespace SpeedMeasuremetRazor.Pages.Measurements
 {
     public class IndexModel : PageModel
     {
         public ISpeedMeasurementRepo SpeedMeasurementRepo { get; set; }
+        public List<SpeedMeasurement> FilterList { get; set; }
 
         public IndexModel(ISpeedMeasurementRepo speedMeasurementRepo)
         {
             SpeedMeasurementRepo = speedMeasurementRepo;
+            FilterList = SpeedMeasurementRepo.GetAllSpeedMeasurements();
         }
 
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPostFilter(int option)
+        {
+            switch (option)
+            {
+                //case 1:
+                //    break;
+                case 2:
+                    Predicate<SpeedMeasurement> predicateOver = (SpeedMeasurement s) => { return s.Speed > s.Location.SpeedLimit; };
+                    FilterList = SortFilter.Comparer(FilterList, predicateOver);
+                    break;
+                case 3:
+                    Predicate<SpeedMeasurement> predicateCut = s => { return s.Speed > s.Location.SpeedLimit * 1.3; };
+                    FilterList = SortFilter.Comparer(FilterList, predicateCut);
+                    break;
+                case 4:
+                    Predicate<SpeedMeasurement> predicateConditional = s =>
+                    {
+                        if (s.Location.Zone == Zone.Motorvej)
+                        {
+                            return s.Speed > s.Location.SpeedLimit * 1.3;
+                        }
+                        else
+                        {
+                            return s.Speed > s.Location.SpeedLimit * 1.6;
+                        }
+                    };
+                    FilterList = SortFilter.Comparer(FilterList, predicateConditional);
+                    break;
+                case 5:
+                    Predicate<SpeedMeasurement> predicateUnconditional = s =>
+                    {
+                        return s.Speed > s.Location.SpeedLimit * 2 && s.Speed >= 100;
+                    };
+                    FilterList = SortFilter.Comparer(FilterList, predicateUnconditional);
+                    break;
+                default:
+                    break;
+            }
+            return Page();
         }
 
         public IActionResult OnPost(int? id)
